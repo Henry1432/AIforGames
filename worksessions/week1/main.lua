f4bc9c5c6ec930c6cbe37fd5a64329982e44b755
Henry Bennett <hbennett@mtl.local>
2023-08-29 20:07:00 -0400

    added week1

---
+++ worksessions/week1/main.lua
@@ -0,0 +1,76 @@
+local agents = {}
+
+function love.load ()
+	for i = 1, 500 do
+		table.insert (agents, 
+		{
+			x=64, y=64,
+			radius = 8,
+			vx = (math.random() * 2 - 1) * 150, vy = (math.random() * 2 - 1) * 150,
+			crowded = false,
+			proximity = 12,
+		})
+	end
+end
+
+function love.update (dt)
+	local wWidth = love.graphics.getWidth()
+	local wHeight = love.graphics.getHeight()
+	for i, agent in ipairs (agents) do
+		agent.x = agent.x + agent.vx * dt
+		agent.y = agent.y + agent.vy * dt
+		agent.x = agent.x % wWidth
+		agent.y = agent.y % wHeight
+		
+		
+		local count = 0
+		for j, agent2 in ipairs (agents) do
+			if (agent2.x < agent.x + agent.proximity) then
+				if (agent2.x > agent.x - agent.proximity) then
+					if (agent2.y < agent.y + agent.proximity) then
+						if (agent2.y > agent.y - agent.proximity) then
+							count = count + 1
+							
+							--local dx = agent2.x - agent.x
+							--local dy = agent2.y - agent.y
+							
+							--if (dx > 0 and vx >0) then
+							--	agent.vx = agent.vx *-1
+							--end
+							--if (dx < 0 and vx < 0) then
+							--	agent.vx = agent.vx*-1
+							--end
+							--if (dy > 0 and vy >0) then
+							--	agent.vy = agent.vy *-1
+							--end
+							--if (dy < 0 and vy < 0) then
+							--	agent.vy = agent.vy*-1
+							--end
+						end
+					end
+				end
+			end
+		end
+		
+		if (count > 4) then
+			agent.crowded = true
+		else
+			agent.crowded = false
+		end
+	end
+
+end
+
+function love.draw ()
+	for i, agent in ipairs (agents) do
+		if agent.crowded then
+			love.graphics.setColor(1,0,0)
+		else
+			love.graphics.setColor(1,1,1)
+		end
+		love.graphics.circle( "fill", agent.x, agent.y, 10)
+		
+			
+	end
+end
+
